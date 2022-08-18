9690d66f74ee54c0b37e64dad85dd9d246eb415d
Daniel Nwaeke <dannie005@gmail.com>
2022-08-18 17:32:38 +0300

    used a ppagination method

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -7,8 +7,13 @@ use App\models\ListItem;
 
 class TodoListController extends Controller
 {
-    public function index() {
-        return view('welcome', [ 'listItems' => ListItem::where('is_complete', 0)->get() ]);
+    public function index(Request $request) {
+        if(($request->filter ?? null) == 'all' ) {
+            $listItems = ListItem::paginate(20);
+        } else {
+            $listItems = ListItem::where('is_complete', true)->paginate(20);
+        }
+        return view('welcome', [ 'listItems' => $listItems ]);
     }
 
     public function completeItem($id){
