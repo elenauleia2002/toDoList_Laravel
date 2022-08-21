6fbccf161a7ef4c5e7b6694a935a4f91817c14d7
Daniel Nwaeke <dannie005@gmail.com>
2022-08-21 20:25:18 +0300

    edited the migrations file to put 'is_complete' as boolean instead of an integer

--- database/migrations/2022_08_08_095147_create_list_items_table.php
+++ database/migrations/2022_08_08_095147_create_list_items_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('list_items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('is_complete');
+            $table->boolean('is_complete');
             $table->timestamps();
         });
     }
